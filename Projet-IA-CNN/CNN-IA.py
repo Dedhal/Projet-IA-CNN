fa053e358fcdb6a00600a50f345ce3a8489d6656
Dedhal <Dedhal@DEDHAL-STATION>
2023-05-08 20:02:06 +0200

    correctif

--- Projet-IA-CNN/CNN-IA.py
+++ Projet-IA-CNN/CNN-IA.py
@@ -110,26 +110,26 @@ print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT 
 model = Sequential()
 
 model.add(Conv2D(64, kernel_size=(3,3), input_shape=(224,224,3), activation='relu', padding='same'))
-model.add(Conv2D(64, (3,3), activation='relu', padding='same'))
+#model.add(Conv2D(64, (3,3), activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
 
 model.add(Conv2D(128, (3,3), activation='relu', padding='same'))
-model.add(Conv2D(128, (3,3), activation='relu', padding='same'))
+#model.add(Conv2D(128, (3,3), activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
 
 model.add(Conv2D(256, (3,3), activation='relu', padding='same'))
 model.add(Conv2D(256, (3,3), activation='relu', padding='same'))
-model.add(Conv2D(256, (3,3), activation='relu', padding='same'))
+#model.add(Conv2D(256, (3,3), activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
 
 model.add(Conv2D(512, (3,3), activation='relu', padding='same'))
 model.add(Conv2D(512, (3,3), activation='relu', padding='same'))
-model.add(Conv2D(512, (3,3), activation='relu', padding='same'))
+#model.add(Conv2D(512, (3,3), activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
 
 model.add(Conv2D(512, (3,3), activation='relu', padding='same'))
 model.add(Conv2D(512, (3,3), activation='relu', padding='same'))
-model.add(Conv2D(512, (3,3), activation='relu', padding='same'))
+#model.add(Conv2D(512, (3,3), activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
 
 model.add(Flatten())
